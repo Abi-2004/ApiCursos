28078268e972150e24a7a2745d33ce0a873d1d0e
Abi-2004 <abiraldahal38@gmail.com>
2024-10-10 09:18:32 +0000

    aaa

---
+++ src/Controller/CursoController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CursoController extends AbstractController
+{
+    #[Route('/curso', name: 'app_curso', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/CursoController.php',
+        ]);
+    }
+}
+
